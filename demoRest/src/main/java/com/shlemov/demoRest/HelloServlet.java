1c38d159d511d522b9e6d6985e86ee9ebf0cafcb
ShlemovV <sslbacyhadls@gmail.com>
2022-07-17 13:39:40 +0500

    rewrite java app

--- demoRest/src/main/java/com/shlemov/demoRest/HelloServlet.java
+++ demoRest/src/main/java/com/shlemov/demoRest/HelloServlet.java
@@ -1,18 +1,11 @@
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 
-@WebServlet("/")
 public class HelloServlet extends HttpServlet {
-
-   @Override
    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       resp.setContentType("text/html");
-       PrintWriter printWriter = resp.getWriter();
-       printWriter.write("Hello!");
-       printWriter.close();
+     String path = "/WEB-INF/view/index.html";
+     req.getRequestDispatcher(path).forward(req, resp);
    }
 }
